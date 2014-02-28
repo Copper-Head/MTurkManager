50909ce9df277d72d7ef8b78b532f5cf5d113336
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2014-02-28 09:37:08 -0500

    added and tested answers file parsing

--- mturk_manager.py
+++ mturk_manager.py
@@ -12,6 +12,68 @@ import re
 from boto.mturk.question import *
 
 
+class AnswerKey(ValidatingXML, list):
+    schema_url = 'http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/AnswerKey.xsd'
+    xml_template = '''<?xml version="1.0" encoding="UTF-8"?>
+<AnswerKey xmlns="{}">
+    {}
+</AnswerKey>'''
+
+    def get_as_xml(self):
+        items = '\n'.join(item.get_as_xml() for item in self)
+        return self.xml_template.format(self.schema_url, items)
+
+
+class AnswerOption():
+    def __init__(self, question_IDs, score):
+        self.question_IDs = question_IDs
+        self.score = score
+        self.template = '''
+        <AnswerOption>
+        {}
+        </AnswerOption>
+        '''
+
+    def get_as_xml(self):
+        IDs = [SimpleField('SelectionIdentifier', ID)
+               for ID in self.question_IDs]
+        fields = IDs + [SimpleField('AnswerScore', self.score)]
+        return self.template.format('\n'.join(f.get_as_xml()
+                                              for f in fields))
+
+
+class QuestionAnswer(Question):
+    template = '<Question>{}\n{}</Question>'
+
+    def __init__(self, identifier, options):
+        self.identifier = SimpleField('QuestionIdentifier', identifier)
+        #self.answer_options = [opt.get_as_xml() for opt in options]
+        self.answer_options = options
+
+    def get_as_xml(self):
+        return self.template.format(self.identifier.get_as_xml(),
+                                    self.answer_options.get_as_xml())
+
+
+def parse_answer_file(file_name):
+    q_rgx = re.compile('Question (.*)')
+    score_rgx = re.compile('Score (.*)')
+    ans_rgx = re.compile('(Answer.+?)(?=Score)', flags=re.DOTALL)
+    a_rgx = re.compile('Answer (.+)')
+    with open(file_name, 'rU') as ans_file:
+        ans_file_str = ans_file.read()
+        questions = q_rgx.findall(ans_file_str)
+        scores = score_rgx.findall(ans_file_str)
+        answers = ans_rgx.findall(ans_file_str)
+        assert len(questions) == len(answers) == len(scores)
+    question_list = []
+    for indx in xrange(len(questions)):
+        q_answers = a_rgx.findall(answers[indx])
+        answer_opts = AnswerOption(q_answers, scores[indx])
+        question_list.append(QuestionAnswer(questions[indx], answer_opts))
+    return AnswerKey(question_list)
+
+
 def parse_question_file(file_name):
     q_rgx = re.compile('Question (\d+) (.+)\s*(.+)')
     a_rgx = re.compile('Answer (.+)\s*(.+)')
@@ -20,8 +82,6 @@ def parse_question_file(file_name):
         q_file_str = q_file.read()
         questions = q_rgx.findall(q_file_str)
         answers = ans_rgx.findall(q_file_str)
-        #print questions
-        #print answers
         assert len(questions) == len(answers)
     question_list = []
     for indx in xrange(len(questions)):
@@ -45,7 +105,8 @@ def parse_question_file(file_name):
 #================================= __MAIN__ ===================================
 def main():
     with open('test.xml', 'w') as test:
-        test.write(parse_question_file('english.questions').get_as_xml())
+        #test.write(parse_question_file('english.questions').get_as_xml())
+        test.write(parse_answer_file('english.answers').get_as_xml())
 
 #------------------------------------------------------------------------------
 if __name__ == '__main__':
